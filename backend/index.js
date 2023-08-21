91184b55e7545b160806e64713f6000b5d51ceb4
Muhammad Sarmad Saleem <sarmadsaleem.333@gmail.com>
2023-08-21 16:27:15 +0500

    Made minute changes

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,10 @@ app.use(express.json());
 app.use(cors());
 app.use('/learning-platform/user-auth', require('./Routes/user-auth'));       //providing route for  user authentication 
 app.use('/learning-platform/instructor-auth', require('./Routes/instructor-auth'));       //providing route for instructor authentication 
-// app.use('/blogging/posts', require('./Routes/posts'));       //providing route for posts
+// app.use('/learning-platform/course-uploading', require('./Routes/course-uploading'));      //course making by the instructor
 // app.use('/blogging/reaction', require('./Routes/reaction'));       //providing route for reaction
 // app.use('/blogging/search', require('./Routes/search'));  // providing route for searching
-// app.use(express.static(path.resolve(__dirname, './public')));  //path for videos(lectures) upload 
+app.use(express.static(path.resolve(__dirname, './public')));  //path for videos images and pdfs upload 
 
 app.listen(port, () => {
     console.log(`The Online Learning Platform is listening on port http://localhost:${port}`);
